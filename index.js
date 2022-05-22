e40d5898fbd6f36d84a017c4640684808016d6fd
Kelvin.m <kelvinmurithi18@gmail.com>
2022-05-22 14:22:18 +0300

    make it better

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const { result } = require('lodash');
 const { render } = require('ejs');
 const courseRoutes = require('./routes/courseroutes');
 //const { findById } = require('./models/course');
+const PORT = process.env.PORT || 5000
 
 const app = express();
 
 //connect to database (mongo)
 const dbUri ='mongodb+srv://Kelvin:PV0wePEstadg1Wrr@nodelearning.1zq04.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(dbUri,  {useNewUrlParser: true, useUnifiedTopology: true})
-          .then(console.log('connection successful'))
+          .then((result)=>app.listen(PORT, () => console.log(`Listening on ${ PORT }`)))
           .catch((err) => console.log(err));
 
 //register view engine
